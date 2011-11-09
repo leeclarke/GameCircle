f534c705a4c78bd4b5fe649b2f6652f2674ec695
Lee <lee.k.clarke@gmail.com>
2011-11-09 16:27:32 -0500

    Added stubbed Adventure controller support, untested

--- GameCircle/app/controllers/AdventureService.java
+++ GameCircle/app/controllers/AdventureService.java
@@ -1,10 +1,26 @@
 package controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import models.Adventure;
+import models.render.AdventureResource;
+import models.util.AdventureDataMapper;
+import models.util.ErrorMessages;
+import models.util.FieldError;
+import play.data.validation.Error;
+import play.data.validation.Validation;
+import exception.JSONException;
 
 /**
  * Responsible for Functions surrounding building and maintaining Adventures. All functionality 
@@ -12,7 +28,7 @@ import javax.ws.rs.WebApplicationException;
  * @author lee
  */
 @Path("/adventure")
-public class AdventureService { 
+public class AdventureService extends GameCircleService{ 
 
 	/**
 	 * Returns the current conditions in JSON format
@@ -26,13 +42,102 @@ public class AdventureService {
 	@GET
 	@Produces("application/json")
 	@Path("/{id}")
-	public String getAdventure(@PathParam("id") final String responseString){
-	    if(responseString.equalsIgnoreCase("boom")){
-	        throw new WebApplicationException(new IllegalArgumentException("2. Bad argument"), 404);
-	    }else if(responseString.equalsIgnoreCase("splat")){
-            throw new WebApplicationException(new IllegalArgumentException("2. Bad argument"), 500);
-        }
+	public String getAdventure(@PathParam("id") final String id){
+	    Adventure adventure = Adventure.findById(id);
 	    
-	    return "{greet:'Hello REST'}";
+	    if (adventure == null)
+        {
+            throw new WebApplicationException(new IllegalArgumentException("Bad argument"), 404);
+        } else
+        {
+    	    AdventureResource advResource = new AdventureResource(adventure);
+    	    return toJSONString(advResource);
+        }
 	}
+	
+	@PUT
+	@Consumes("application/x-www-form-urlencoded")
+    @Produces("application/json")
+    public Response addAdventure(MultivaluedMap<String, String> formParams){
+	    Adventure newAdv = AdventureDataMapper.buildAdventure(formParams);
+        if (!isExistingAdventure(newAdv))
+        {
+            try
+            {
+                validateAdventure(newAdv);
+            } catch (JSONException jsonE)
+            {
+                return sendError(jsonE.errors);
+            }
+            newAdv.save();
+        } else
+        {
+            return sendError(404,new FieldError("userName","User ID already in use.",true));
+        }
+        
+	    Map<String, String> params = new HashMap<String, String>();
+        params.put("id", String.valueOf(newAdv.id));
+        
+	    return sendRedirect("adventure", "get-adventure", params);
+	}
+	
+	/**
+     * Checks to see if uses already exists.
+     * 
+     * @param newUser
+     * @throws WebApplicationException
+     * @throws JSONException 
+     */
+    private boolean isExistingAdventure(Adventure newAdv) throws WebApplicationException
+    {
+        boolean exists = true;
+        if(newAdv == null || newAdv.id != null){
+            return false;
+        }
+        //TODO: What would indicase a unique value for an Adventure? Name and User? Does this need checking?
+//        Adventure adv = Adventure.findById(id)(newUser.userName);
+//        if (user == null)
+//        {
+//            exists = false;
+//        }
+        return exists;
+    }
+    
+    /**
+     * Validates User, throws Exceptions if invalid user.
+     * 
+     * @param newUser
+     * @throws WebApplicationException
+     * @throws JSONException
+     */
+    private void validateAdventure(Adventure newAdv) throws WebApplicationException, JSONException
+    {
+        int status = 303;
+        Validation validation = Validation.current();
+        if (newAdv == null) {
+            validation.addError("userName", "Invalid State", "");
+            status = 404;
+        }
+        //TODO: Replace user example with actual validation.
+//        validation.email(newUser.email);
+//        validation.minSize("firstName", newUser.firstName, 2);
+//        boolean updateSuccess = newUser.validateAndSave(); 
+//        
+//        //Catch a save error that wasn't from validation.
+//        if(!updateSuccess && !validation.hasErrors()){
+//            validation.addError("user", "Unable to update User data.", "");
+//        }
+        
+        ErrorMessages errors = new ErrorMessages();
+        if (validation.hasErrors())
+        {
+            status = 409;
+            for (Error err : validation.errors()) {
+                errors.errors.add(new FieldError(err.getKey(), err.toString(), false));
+            }
+            
+             throw new JSONException(status, errors);
+        }
+    }
+    
 }
---
+++ GameCircle/app/models/util/AdventureDataMapper.java
@@ -0,0 +1,14 @@
+package models.util;
+
+import javax.ws.rs.core.MultivaluedMap;
+
+import models.Adventure;
+
+public class AdventureDataMapper {
+
+    public static Adventure buildAdventure(MultivaluedMap<String, String> formParams) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
